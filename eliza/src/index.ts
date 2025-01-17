89ac9cc4c43a40f567beb4ecde4c8d3f0b6d8e6c
pis <piscarefree@gmail.com>
2025-01-17 14:55:55 +0800

    chore: modify index

--- eliza/src/index.ts
+++ eliza/src/index.ts
@@ -1,11 +1,11 @@
 import { Plugin } from "@elizaos/core";
-import { databaseProvider } from "./providers/ethereum/database";
+import { ethereumDataProvider } from "./providers/ethereum/database";
 
 export const onchainDataPlugin: Plugin = {
     name: "onchain data plugin",
     description: "Enables onchain data fetching",
     actions: [],
-    providers: [databaseProvider],
+    providers: [ethereumDataProvider],
     evaluators: [],
     services: [],
     clients: [],
